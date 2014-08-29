9bdd2dd587482eedca6adc6a08b750ffc5871c51
bill <gbtian@gmail.com>
2014-08-29 00:53:41 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -493,7 +493,7 @@ int update_path_stat_delay(unsigned char *node_id, unsigned char path_id, u32 de
 	if (path_stat)
 	{
 		getnstimeofday(&tv);
-		midtime = (tv.tv_sec % 86400) * MSEC_PER_SEC  + tv.tv_nsec / NSEC_PER_MSEC;
+		midtime = (tv.tv_sec % 86400) * MSEC_PER_SEC * 10  + (tv.tv_nsec * 10) / NSEC_PER_MSEC;
 
 		path_stat->delay = midtime - delay;
 		path_stat->feedbacked = false;
@@ -526,27 +526,37 @@ int update_path_delay(unsigned char path_id, __s32 delay)
 				//path_info->delay = delay;
 			}
 
-			if (path_info->count < 5)
-			{
-				path_info->min_delay = (4 * path_info->min_delay + delay) / 5;
-				path_info->max_delay = (4 * path_info->max_delay + delay) / 5;
-				//path_info->min_delay = delay;
-				path_info->count += 1;
-			}
-			else
-			{
-				if (path_info->min_delay > path_info->delay)
-				{
-					path_info->min_delay = (99 * path_info->min_delay + delay) / 100;
-					//path_info->min_delay = delay;
-				}
+//			if (path_info->count < 5)
+//			{
+//				path_info->min_delay = (4 * path_info->min_delay + delay) / 5;
+//				path_info->max_delay = (4 * path_info->max_delay + delay) / 5;
+//				//path_info->min_delay = delay;
+//				path_info->count += 1;
+//			}
+//			else
+//			{
+//				if (path_info->min_delay > path_info->delay)
+//				{
+//					path_info->min_delay = delay;
+//				}
+//
+//				if (path_info->max_delay < path_info->delay)
+//				{
+//					path_info->max_delay = (99 * path_info->max_delay + delay) / 100;
+//					//path_info->max_delay = delay;
+//				}
+//			}
 
-				if (path_info->max_delay < path_info->delay)
-				{
-					path_info->max_delay = (99 * path_info->max_delay + delay) / 100;
-					//path_info->max_delay = delay;
-				}
+			if (path_info->min_delay > delay)
+			{
+				path_info->min_delay = delay;
 			}
+
+			if (path_info->max_delay < delay)
+			{
+				path_info->max_delay = delay;
+			}
+
 
 			path_info->queuing_delay = path_info->delay - path_info->min_delay;
 			if (path_info->queuing_delay > path_info->max_queuing_delay)
@@ -665,6 +675,23 @@ bool is_in_sorted_list(struct list_head *sorted_list, struct path_info_table *pa
 	return false;
 
 }
+
+int calc_path_similarity(unsigned char session_id)
+{
+	struct path_info_table *path_info;
+
+	if (session_id <= 0)
+		return 0;
+
+	list_for_each_entry(path_info, &pi_head, list)
+	{
+		if (path_info->session_id != session_id)
+			continue;
+	}
+
+	return 0;
+}
+
 int update_path_info(unsigned char session_id, unsigned int len)
 {
 	struct path_info_table *path_info;
@@ -684,7 +711,6 @@ int update_path_info(unsigned char session_id, unsigned int len)
 
 	while(true)
 	{
-//		printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 		struct path_info_table *min_path = NULL;
 		__s32 min_value = -1;
 		list_for_each_entry(path_info, &pi_head, list)
@@ -692,27 +718,22 @@ int update_path_info(unsigned char session_id, unsigned int len)
 			if (path_info->session_id != session_id)
 				continue;
 
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			if (!is_in_sorted_list(&sorted_list, path_info))
 			{
 				if (path_info->delay < min_value || min_value == -1)
 				{
 					min_value = path_info->delay;
 					min_path = path_info;
-//					printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 				}
 			}
 		}
 
-//		printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 		if (min_path != NULL)
 		{
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			struct sort_path *item = kzalloc(sizeof(struct sort_path),	GFP_ATOMIC);
 			if (!item)
 				break;
 
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			item->path_info = min_path;
 			INIT_LIST_HEAD(&(item->list));
 			list_add(&(item->list), &(sorted_list));
@@ -724,23 +745,17 @@ int update_path_info(unsigned char session_id, unsigned int len)
 
 	struct sort_path *sp = NULL;
 	struct sort_path *next_sp = NULL;
-//	printk("%d: %s, %s, %d\n", count, __FILE__, __FUNCTION__, __LINE__);
 	if (count == 4)
 	{
 		list_for_each_entry(sp, &sorted_list, list)
 		{
-//			printk("%d: %s, %s, %d\n", sp->path_info->path_id, __FILE__, __FUNCTION__, __LINE__);
 			next_sp = list_entry(sp->list.next, typeof(*sp), list);
 			if(next_sp)
 			{
 				sp->path_info->ave_max_delay = next_sp->path_info->ave_max_delay =
 										(sp->path_info->delay + next_sp->path_info->delay) / 2;
 
-//				printk("%d: %s, %s, %d\n", next_sp->path_info->path_id, __FILE__, __FUNCTION__, __LINE__);
-
 				sp = list_entry(sp->list.next, typeof(*sp), list);
-
-//				printk("%d: %s, %s, %d\n", sp->path_info->path_id, __FILE__, __FUNCTION__, __LINE__);
 			}
 		}
 	}
@@ -749,8 +764,6 @@ int update_path_info(unsigned char session_id, unsigned int len)
 		list_for_each_entry(sp, &sorted_list, list)
 		{
 			sp->path_info->ave_max_delay = sp->path_info->delay;
-//			printk("%d: %s, %s, %d\n", sp->path_info->path_id, __FILE__, __FUNCTION__, __LINE__);
-
 		}
 	}
 
@@ -768,7 +781,6 @@ int update_path_info(unsigned char session_id, unsigned int len)
 
 	while(true)
 	{
-//		printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 		struct path_info_table *min_path = NULL;
 		__s32 min_value = -1;
 		list_for_each_entry(path_info, &pi_head, list)
@@ -776,27 +788,22 @@ int update_path_info(unsigned char session_id, unsigned int len)
 			if (path_info->session_id != session_id)
 				continue;
 
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			if (!is_in_sorted_list(&sorted_list_1, path_info))
 			{
 				if (path_info->min_delay < min_value || min_value == -1)
 				{
 					min_value = path_info->min_delay;
 					min_path = path_info;
-//					printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 				}
 			}
 		}
 
-//		printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 		if (min_path != NULL)
 		{
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			struct sort_path *item = kzalloc(sizeof(struct sort_path),	GFP_ATOMIC);
 			if (!item)
 				break;
 
-//			printk("%s, %s, %d\n", __FILE__, __FUNCTION__, __LINE__);
 			item->path_info = min_path;
 			INIT_LIST_HEAD(&(item->list));
 			list_add(&(item->list), &(sorted_list_1));
@@ -810,7 +817,6 @@ int update_path_info(unsigned char session_id, unsigned int len)
 	{
 		list_for_each_entry(sp, &sorted_list_1, list)
 		{
-//			printk("%d: %s, %s, %d\n", sp->path_info->path_id, __FILE__, __FUNCTION__, __LINE__);
 			next_sp = list_entry(sp->list.next, typeof(*sp), list);
 			if(next_sp)
 			{
@@ -895,10 +901,10 @@ int update_path_info(unsigned char session_id, unsigned int len)
 			if (max_delay < 0)
 				max_delay = -max_delay;
 
-			//int tmp = len * max_queuing_delay / (diff1+sysctl_mpip_bw_4);
-			int tmp = len * (max_queuing_delay - diff1) / sysctl_mpip_bw_4;
-			//tmp *= (1550 - len) * max_delay / (diff2+sysctl_mpip_bw_4);
-			tmp *= (1550 - len) * (max_delay - diff2) / sysctl_mpip_bw_4;
+			//similarity, the smaller, the more similar
+			int similarity = calc_path_similarity();
+			int tmp = (similarity * max_queuing_delay) / diff1
+								+ (max_delay * similarity) / diff2;
 
 			path_info->bw = (999 * path_info->bw + tmp) / 1000;
 		}
