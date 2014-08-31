bb053c7c8a202dde364a7aa4ee240761dc7de088
bill <gbtian@gmail.com>
2014-08-31 18:21:20 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -990,7 +990,7 @@ int update_path_info(unsigned char session_id, unsigned int len)
 		{
 			__u64 highbw = get_path_bw(path_info->path_id, session_id, path_info->bw);
 
-			path_info->bw = path_info->bw / 2 + highbw / 2;
+			path_info->bw = path_info->bw / 3 + (2 * highbw) / 3;
 		}
 	}
 
