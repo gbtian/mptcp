7fa2c7bf926da3e21e07cb2be34cb6c75a627b0f
bill <gbtian@gmail.com>
2014-09-11 21:18:24 -0400

    submit

--- utility/addmpiprouterule.c
+++ utility/addmpiprouterule.c
@@ -19,7 +19,9 @@
 
 int main(int argc, char **argv)
 {
-//	printf("%s,%s,%s,%d,%d,%d,%d\n",argv[0], );
+	printf("%s,%s,%s,%d,%d,%d,%d\n",argv[0], argv[1], argv[2],
+			atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
+
 	return syscall(316, argv[1], argv[2],
 				atoi(argv[3]), atoi(argv[4]), atoi(argv[5]), atoi(argv[6]));
 }
