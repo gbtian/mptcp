f18832f8aa04a745ba3ba354ed7106e9da813228
bill <gbtian@gmail.com>
2014-09-16 01:19:52 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -2091,8 +2091,8 @@ void write_mpip_log_to_file(unsigned char session_id)
 			printk("create file error\n");
 			return;
 		}
-		old_fs = get_fs();
-		set_fs(KERNEL_DS);
+//		old_fs = get_fs();
+//		set_fs(KERNEL_DS);
 
 		list_for_each_entry(mpip_log, &(path_info->mpip_log), list)
 		{
@@ -2108,7 +2108,7 @@ void write_mpip_log_to_file(unsigned char session_id)
 		}
 
 		filp_close(fp, NULL);
-		set_fs(old_fs);
+//		set_fs(old_fs);
 
 		list_for_each_entry_safe(mpip_log, tmp_mpip, &(path_info->mpip_log), list)
 		{
