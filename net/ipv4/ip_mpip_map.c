3aa48b2e7ea222e4e31d27a4fc190613ac427bba
bill <gbtian@gmail.com>
2014-09-17 21:42:24 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -2087,37 +2087,45 @@ void write_mpip_log_to_file(unsigned char session_id)
 			continue;
 		}
 
-		old_fs = get_fs();
-		set_fs(KERNEL_DS);
+//		old_fs = get_fs();
+//		set_fs(KERNEL_DS);
 
-		char filename[100];
-		sprintf(filename, "/home/bill/%d_%d.csv", session_id, path_info->path_id);
-
-		fp = filp_open(filename, O_RDWR | O_APPEND | O_CREAT, 0644);
-		if (IS_ERR(fp))
-		{
-			printk("create file error\n");
-			return;
-		}
+//		char filename[100];
+//		sprintf(filename, "/home/bill/%d_%d.csv", session_id, path_info->path_id);
+//
+//		fp = filp_open(filename, O_RDWR | O_APPEND | O_CREAT, 0644);
+//		if (IS_ERR(fp))
+//		{
+//			printk("create file error\n");
+//			return;
+//		}
 
 		list_for_each_entry(mpip_log, &(path_info->mpip_log), list)
 		{
-			char buf[100];
-			sprintf(buf, "%lu,%d,%d,%lu\n", mpip_log->logjiffies,
+
+			printk("%lu,%d,%d,%lu\n", mpip_log->logjiffies,
 										mpip_log->delay,
 										mpip_log->queuing_delay,
 										mpip_log->tp);
 
-			pos = fp->f_dentry->d_inode->i_size;
-			vfs_write(fp, buf, strlen(buf), &pos);
+
+//			char buf[100];
+//			sprintf(buf, "%lu,%d,%d,%lu\n", mpip_log->logjiffies,
+//										mpip_log->delay,
+//										mpip_log->queuing_delay,
+//										mpip_log->tp);
+
+
+//			pos = fp->f_dentry->d_inode->i_size;
+//			vfs_write(fp, buf, strlen(buf), &pos);
 //			fp->f_op->write(fp, buf, strlen(buf), &(fp->f_pos));
-			vfs_fsync(fp, 0);
-			printk("%s, %d, %d\n", filename, pos, fp->f_pos);
+//			vfs_fsync(fp, 0);
+//			printk("%s, %d, %d\n", filename, pos, fp->f_pos);
 		}
 
-		set_fs(old_fs);
-
-		filp_close(fp, NULL);
+//		set_fs(old_fs);
+//
+//		filp_close(fp, NULL);
 
 		list_for_each_entry_safe(mpip_log, tmp_mpip, &(path_info->mpip_log), list)
 		{
