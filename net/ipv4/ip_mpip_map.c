6dcbbf1a009794e8f2906851d822df0e75e410aa
bill <gbtian@gmail.com>
2014-09-24 17:20:02 -0400

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -1083,7 +1083,7 @@ int update_path_info(unsigned char session_id, unsigned int len)
 //			tmp = max_queuing_delay - path_info->ave_queuing_delay;
 //		}
 
-		int tmp = max_delay - path_info->ave_delay;
+		int tmp = max_delay - path_info->delay;
 
 		path_info->tmp = tmp;
 
