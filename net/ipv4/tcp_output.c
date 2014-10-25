9d338c6806ed95b599cf6e8949f0622a0fa3f7c2
bill <gbtian@gmail.com>
2014-10-24 22:38:20 -0400

    submit

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -1435,7 +1435,7 @@ unsigned int tcp_current_mss(struct sock *sk)
 	if (sysctl_mpip_enabled &&
 		is_mpip_enabled(inet_sk(sk)->inet_daddr, inet_sk(sk)->inet_dport))
 	{
-		printk("%d, %s, %s, %d\n", mss_now, __FILE__, __FUNCTION__, __LINE__);
+//		printk("%d, %s, %s, %d\n", mss_now, __FILE__, __FUNCTION__, __LINE__);
 		mss_now -= ((MPIP_CM_LEN + 2 + 3) & ~3);
 	}
 
