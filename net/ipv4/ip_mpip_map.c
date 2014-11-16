d1b35edd304f0db8045a4ac58159313191e3a883
bill <gbtian@gmail.com>
2014-11-16 14:40:17 -0500

    submit

--- net/ipv4/ip_mpip_map.c
+++ net/ipv4/ip_mpip_map.c
@@ -603,8 +603,8 @@ int update_path_delay(unsigned char path_id, __s32 delay)
 			}
 			else
 			{
-//				path_info->delay = (4 * path_info->delay + delay) / 5;
-				path_info->delay = delay;
+				path_info->delay = (99 * path_info->delay + delay) / 100;
+//				path_info->delay = delay;
 			}
 
 			if (path_info->min_delay > delay || path_info->min_delay == -1)
